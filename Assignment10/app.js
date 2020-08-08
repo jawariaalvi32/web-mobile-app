3b443b842d2a11a12a1e262a5a96b08ecb9c8f3e
Jawaria Alvi <jawariaalvi32@gmail.com>
2020-08-09 01:21:46 +0500

    Assignment #10 - ToDo Application

--- Assignment10/app.js
+++ Assignment10/app.js
@@ -1,52 +1,82 @@
-let myTodo = [];
+var inbox_count = 0;
+function create() {
+    inbox_count++;
+    document.getElementById("toDo").style.display = "block";
 
-function refreshTodo() {
-    let myList = document.getElementById('toDo');
+    var ul = document.getElementById("toDo");
+    var val = document.getElementById("newTodo").value;
 
-    document.getElementById('toDo').innerHTML = '';
+    var li = document.createElement("li");
+    var liText = document.createTextNode(val);
 
-    myTodo.map(({id, task, isComplete}, i) => {
-        let list = document.createElement("li");
-        let clickable = document.createElement("button");
-        clickable.innerHTML = task;
-        clickable.id = `task-${id}`;
-        clickable.className = "btn";
-        clickable.setAttribute("onclick", `updateTodo(event, ${i})`);
-        list.appendChild(clickable);
-        list.className = `list_task ${isComplete ? "done" : "notDone"}`;
-        myList.appendChild(list);
-    });
+    var del = document.createElement("button");
+    del.setAttribute("class", "btn btn-sm btn-light");
+    del.setAttribute("onclick", "del(this)");
+
+    var i = document.createElement("i");
+    i.setAttribute("class", "fa fa-trash");
+
+    var edit = document.createElement("button");
+    edit.setAttribute("class", "btn btn-sm btn-light");
+    edit.setAttribute("onclick", "edit(this)");
+
+    var edit_i = document.createElement("i");
+    edit_i.setAttribute("class", "fa fa-edit");
+
+    var fav = document.createElement("button");
+    fav.setAttribute("class", "btn btn-sm btn-light");
+    fav.setAttribute("onclick", "favs(this)");
+
+    var fav_i = document.createElement("i");
+    fav_i.setAttribute("class", "fa fa-star");
+
+    del.appendChild(i);
+    edit.appendChild(edit_i);
+    fav.appendChild(fav_i);
+
+    li.appendChild(liText);
+    li.appendChild(del);
+    li.appendChild(edit);
+    li.appendChild(fav);
+
+    ul.appendChild(li);
+
+    document.getElementById("newTodo").value = "";
 }
 
-function updateTodo({ target }, i){
-    console.log(target.id, i);
-    let flag = myTodo[i].isComplete;
-    myTodo[i].isComplete = !flag;
-    if(myTodo[i].isComplete){
-        popup();
+function del(e) {
+    inbox_count--;
+    e.parentNode.remove();
+}
+
+function edit(data) {
+    var val = data.parentNode.firstChild.nodeValue;
+    var ip = prompt("Enter the edited value", val);
+
+    data.parentNode.firstChild.nodeValue = ip;
+}
+var fav_count = 0;
+function favs(e) {
+    var inbox_li = document.getElementById("toDo");
+    var fav_li = document.getElementById("fav");
+
+    if( e.style.color == "gold" ) {
+        fav_count--;
+        inbox_count++;
+        inbox_li.appendChild(e.parentNode);
+        e.style.color = "black";
+        if( fav_count == 0 ) {
+            fav_li.style.display = "none";
+        }
+    } else {
+        fav_count++;
+        inbox_count--;
+        fav_li.appendChild(e.parentNode);
+        fav_li.style.display = "block";
+        e.style.color = "gold";
+        if( inbox_count == 0 ) {
+            inbox_li.style.display = "none";
+        }
     }
-    console.log(myTodo[i]);
-    let parent = document.getElementById(target.id).parentElement;
-    parent.className = `list_task ${myTodo[i].isComplete ? "done" : "notDone"}`;
-}
-
-function createTodo({ target }){
-    console.log(target.value);
-    let newTodo = {
-        'id': myTodo.length + 1,
-        'task': target.value,
-        'isComplete': false
-    };
-    target.value = '';
-    console.log(newTodo);
-    myTodo = [...myTodo, newTodo];
-    refreshTodo();
-}
-
-function popup(){
-    document.getElementById('popup').style.cssText = 'display: flex; justify-content: center;';
-    // document.getElementById('popup').innerHTML = "Complete";
-    // setTimeout(() => {
-    //     document.getElementById('popup').style.display = 'none';
-    // }, 1500);
+    
 }
